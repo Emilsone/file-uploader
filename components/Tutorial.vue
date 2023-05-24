5baec545df689cba24ec1dd26bbb9027021231b6
Your Name Here <lawsonezekiel68@gmail.com>
2023-05-24 03:26:32 +0100

    create file layout woith Tailwind CSS

---
+++ components/Tutorial.vue
@@ -0,0 +1,84 @@
+<!-- Please remove this file from your project -->
+<template>
+  <main>
+    <!-- <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0 hero-section">
+      <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+        <div class="text-center justify-center pt-8 sm:pt-0">
+          <h2 class="text-2xl leading-7 font-semibold">
+            Upload your big files
+          </h2>
+          <p class="mt-3 text-gray-600">
+            Simple . Fast. Beautiful </p>
+        </div> -->
+    <!-- <div class="flex bg-white shadow-lg rounded-lg mx-4 md:mx-auto mt-6 max-w-md md:max-w-7/12 ">
+          <div class="flex items-center px-4 py-6">
+            <div class=" text-center">
+              <span class="">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                  stroke="currentColor" class="w-8 h-8 mx-auto my-6">
+                  <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M15 11.25l-3-3m0 0l-3 3m3-3v7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+              </span>
+              <h2 class="text-2xl leading-7 font-semibold">
+                Drop like it's hot
+              </h2>
+              <p class="mt-3 text-gray-600">
+                No limit</p>
+              <p class="mt-4 pt-4 text-gray-800 border-t border-dashed">
+                
+              </p>
+
+            </div>
+          </div>
+        </div> -->
+
+
+
+
+
+    <!-- </div> -->
+    <!-- </div> -->
+    <div class="min-w-screen min-h-screen  flex items-center p-5 lg:p-10 overflow-hidden relative hero-section">
+      <div class="border-8 border-yellow-20 rounded-lg border-dashed  absolute top-20 bottom-20 left-20 right-20 z-0">
+      </div>
+      <div
+        class="w-full max-w-4xl rounded-lg bg-white shadow-xl p-10 lg:p-20 mx-auto text-gray-800 relative md:text-left">
+        <div class="text-center">
+          <div class="text-center px-10">
+            <div class="mb-10">
+              <span class="">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 my-6 mx-auto text-blue-500" fill="none"
+                  viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                  <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
+                </svg>
+              </span>
+              <h2 class="text-2xl leading-7 font-semibold">
+                Drop like it's hot
+              </h2>
+              <p class="mt-3 text-gray-600">
+                No limit</p>
+              <p class="mt-2 text-gray-500 tracking-wide border-t border-dashed ">Upload or darg & drop your file SVG,
+                PNG, JPG or GIF. </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'NuxtTutorial'
+}
+</script>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Work+Sans&display=swap');
+
+.hero-section {
+  font-family: 'Work Sans', sans-serif;
+  background-color: #5E8AD5;
+}
+</style>
